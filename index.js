32b4dbf2256f2f3afbe44e862c66c382704b258e
Pavel Kondratenko <p.kondratenko@rambler-co.ru>
2016-02-04 16:23:19 +0300

    change configuration logic

--- index.js
+++ index.js
@@ -3,19 +3,8 @@
 var _ = require('underscore');
 var Backbone = require('backbone');
 
-var fill = function(object, properties) {
-  for (var name in properties) {
-    if (typeof object[name] !== 'function') {
-      object[name] = properties[name];
-    } else {
-      throw new Error('You can not override the method "' + name + '" via component constructor. For this use "Component.extend()" method.');
-    }
-  }
-};
-
 var Component = Backbone.Component = function(config) {
   this.cid = _.uniqueId('c');
-  config && fill(this, config);
   this.configure.apply(this, arguments);
   this.initialize.apply(this, arguments);
 };
@@ -26,8 +15,19 @@ if (window) {
 }
 
 _.extend(Component.prototype, Backbone.Events, {
-  initialize: function() {},
-  configure: function() {}
+  configure: function(config) {
+    for (var name in config) {
+      if (typeof this[name] !== 'function') {
+        this[name] = config[name];
+      } else {
+        throw new Error('You can not override the method "' + name + '" via component constructor. For this use "Component.extend()" method.');
+      }
+    }
+  },
+  
+  initialize: function(config) {
+    return true;
+  }
 });
 
 Component.extend = Backbone.Model.extend;
